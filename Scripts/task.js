c615128b2723f3135a5ee89c29a2d0a0bb463391
dstanchevici <distanchev@gmail.com>
2025-02-23 16:02:08 -0500

    In task.js, added the start of full screen. Also, added listerners for entering/exiting fullscreen and for clicking on/off the window.

--- Scripts/task.js
+++ Scripts/task.js
@@ -1,9 +1,57 @@
 const divConfirmationBlock = document.getElementById("divConfirmationBlock");
 const divTextAndLinksBlock = document.getElementById("divTextAndLinksBlock");
 
-const btnBegin = document.getElementById("btnBegin");
-btnBegin.addEventListener("click", () => {
+document.getElementById("btnBegin").addEventListener("click", () => {
+  // Change display (visibility) to hide confirmation and display textbox and links.
   divConfirmationBlock.classList.add("d-none");
   divTextAndLinksBlock.classList.remove("d-none");
+
+  // Switch to full screen.
+  let elem = document.documentElement; // This selects the entire HTML document              
+  if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { // Firefox
+      elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { // Chrome, Safari, and Opera
+      elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { // IE/Edge
+      elem.msRequestFullscreen();
+  }
 });
 
+// Listen for attempts to switch out of full screen.
+// In Chrome, this works with esc button, but not with Chrome's enter/exit full screen button.
+// In Safari, it works fine.
+document.addEventListener("fullscreenchange", onFullscreenChange);
+document.addEventListener("webkitfullscreenchange", onFullscreenChange);
+document.addEventListener("mozfullscreenchange", onFullscreenChange);
+document.addEventListener("MSFullscreenChange", onFullscreenChange);
+
+function onFullscreenChange(event) {
+    console.log("Fullscreen change event fired");
+    if (document.fullscreenElement) {
+        console.log("Entered fullscreen mode");
+    } else {
+        console.log("Exited fullscreen mode");
+    }
+}
+
+// Close fullscreen.
+document.getElementById("btnSubmit").addEventListener("click", () => {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) { /* Safari */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE11 */
+    document.msExitFullscreen();
+  }
+});
+
+// List to whether the mouse is clicked inside or outside the window.
+window.addEventListener('blur', function() {
+  console.log('Window lost focus');
+});
+
+window.addEventListener('focus', function() {
+  console.log('Window gained focus');
+});
